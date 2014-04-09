3ce7172a925670751641612ad5eb61382868c1e9
Yuri Buyanov <yuri.buyanov@gmail.com>
2014-04-09 18:41:54 +0400

    Initial Commit

---
+++ client/ISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ISAppDelegate.h
+//  client
+//
+//  Created by Yuriy Buyanov on 09/04/14.
+//  Copyright (c) 2014 Yuriy Buyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ client/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  client
+//
+//  Created by Yuriy Buyanov on 09/04/14.
+//  Copyright (c) 2014 Yuriy Buyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ISAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ISAppDelegate class]));
+    }
+}
